25286f202b9dd58d15a931c16a1a953ce59036e8
Wilmer Mendoza <w4494329@gmail.com>
2022-03-06 16:26:57 -0500

    Prueba

--- co.com.choucair.certification.retot/src/main/java/co/com/choucair/certification/retot/userinterface/RegisterUtestPage.java
+++ co.com.choucair.certification.retot/src/main/java/co/com/choucair/certification/retot/userinterface/RegisterUtestPage.java
@@ -15,4 +15,31 @@ public class RegisterUtestPage extends PageObject {
       .located(By.id("email"));
     public static final Target SELECT_MONTH =Target.the("Register the month of birth")
       .located(By.id("birthMonth"));
+    public static final Target SELECT_DAY =Target.the("Register the day of birth")
+            .located(By.id("birthDay"));
+    public static final Target SELECT_YEAR =Target.the("Register the year of birth")
+            .located(By.id("birthYear"));
+    public static final Target BUTTON_LOCATION =Target.the("Button to go to the location page")
+            .located(By.xpath("//a[@class='btn btn-blue']"));
+    public static final Target INPUT_CITY =Target.the("Register the city where you live")
+            .located(By.id("city"));
+    public static final Target INPUT_ZIP =Target.the("Enter the postal code of the city where you live")
+            .located(By.id("zip"));
+    public static final Target BUTTON_DEVICES =Target.the("Button to go to the device page")
+            .located(By.xpath("//div[@class='pull-right next-step']"));
+    public static final Target BUTTON_LAST =Target.the("Button to go to the last page")
+            .located(By.xpath("//a[@class='btn btn-blue pull-right']"));
+    public static final Target INPUT_PASSWORD =Target.the("Password creation")
+            .located(By.id("password"));
+    public static final Target INPUT_VERIPASSWORD =Target.the("Password verification")
+            .located(By.id("confirmPassword"));
+    public static final Target CHEK_STAY =Target.the("Acceptance of terms of receiving notifications")
+            .located(By.xpath("//span[@class='checkmark signup-consent__checkbox signup-consent__checkbox--highlight']"));
+    public static final Target CHEK_USE =Target.the("Acceptance of the conditions of use and conduct")
+            .located(By.xpath("//span[@class='checkmark signup-consent__checkbox error']"));
+    public static final Target CHEK_PRIVACY =Target.the("Acceptance of the privacy and security conditions")
+            .located(By.xpath("//span[@class='checkmark signup-consent__checkbox error']"));
+    public static final Target FINAL_PAGE =Target.the("Check the name of the final page of the form")
+            .located(By.xpath("//span[contains(text(),'The last step')]"));
+
 }
--- co.com.choucair.certification.retot/src/test/java/co/com/choucair/certification/retot/stepdefinitions/UtestStepDefinitions.java
+++ co.com.choucair.certification.retot/src/test/java/co/com/choucair/certification/retot/stepdefinitions/UtestStepDefinitions.java
@@ -1,12 +1,14 @@
 package co.com.choucair.certification.retot.stepdefinitions;
 
 import co.com.choucair.certification.retot.model.UtestData;
+import co.com.choucair.certification.retot.questions.Answer;
 import co.com.choucair.certification.retot.tasks.OpenUpUtest;
 import co.com.choucair.certification.retot.tasks.Register;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import net.serenitybdd.screenplay.GivenWhenThen;
 import net.serenitybdd.screenplay.actors.OnStage;
 import net.serenitybdd.screenplay.actors.OnlineCast;
 
@@ -28,12 +30,13 @@ public class UtestStepDefinitions {
     public void heFillsInTheFormForTheCreationOfANewUser(List<UtestData> data) throws Exception{
         OnStage.theActorInTheSpotlight().attemptsTo(Register.the(data.get(0).getFirts_name()
                 ,data.get(0).getLast_name()
-                ,data.get(0).getEmail()));
+                ,data.get(0).getEmail()
+                ,data.get(0).getPassword()));
     }
 
-    @Then("^access the Utest platform$")
-    public void accessTheUtestPlatform() {
-
+    @Then("^get to the end of the registration form$")
+    public void getToTheEndOfTheRegistrationForm(List<UtestData>data) throws Exception {
+        OnStage.theActorInTheSpotlight().should(GivenWhenThen.seeThat(Answer.toThe(data.get(0).getQuestion())));
     }
 
 }
